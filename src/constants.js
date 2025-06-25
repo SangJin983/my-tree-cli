497d4b0f1b5b34f29941fd3d3867acd1ee899426
SangJin983 <lazistar983@gmail.com>
2025-06-25 17:02:11 +0900

    feat: TreeNode data model 정의 및 node type 상수 추가

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const NODE_TYPES = Object.freeze({
+  FILE: 'file',
+  DIRECTORY: 'directory',
+});
---
+++ src/data/tree.model.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef {Object} TreeNode
+ * @property {'file' | 'directory'} type - 노드의 타입
+ * @property {string} name - 파일 또는 폴더의 이름
+ * @property {string} path - 전체 경로
+ * @property {number} level - 트리의 깊이
+ * @property {TreeNode[]} [children] - 자식 노드 배열 (폴더인 경우에만)
+ */
+export {}; // 명시적인 모듈 선언
